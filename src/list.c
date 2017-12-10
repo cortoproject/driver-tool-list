099cad3c0fc78ecc8288689a4afad47c55efced7
Sander Mertens <sander.mertens8@gmail.com>
2017-12-09 17:09:26 -0800

    Change to v2.0

--- src/list.c
+++ src/list.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2017 the corto developers
+/* Copyright (c) 2010-2018 the corto developers
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,87 +24,34 @@
 #include <driver/tool/list/list.h>
 
 int cortomain(int argc, char *argv[]) {
-    corto_ll locate, project;
 
     CORTO_UNUSED(argc);
 
-    corto_argdata *data = corto_argparse(
-      argv,
-      (corto_argdata[]){
-        /* Ignore first argument */
-        {"$0", NULL, NULL},
-        {"--locate", &locate, NULL},
-
-        /* Match at most one project directory */
-        {"$?*", &project, NULL},
-        {NULL}
-      }
-    );
-
-    if (project) {
-        corto_chdir(corto_ll_get(project, 0));
-        corto_ll packages = corto_loadGetPackages();
-        if (packages && corto_ll_size(packages)) {
-            corto_iter iter = corto_ll_iter(packages);
-            while (corto_iter_hasNext(&iter)) {
-                corto_string str = corto_iter_next(&iter);
-                corto_string package = corto_locate(str, NULL, CORTO_LOCATION_LIB);
-                if (package) {
-                    printf("  %s%s%s  =>  %s\n", CORTO_CYAN, str, CORTO_NORMAL, package);
-                } else {
-                  printf("  %s%s%s  =>  %smissing%s\n", CORTO_CYAN, str, CORTO_NORMAL, CORTO_RED, CORTO_NORMAL);
-                }
-            }
+    corto_iter it;
+    corto_int32 count = 0;
+    if (corto_select("//").type("/corto/vstore/package").iter(&it)) {
+        goto error;
+    }
+    while (corto_iter_hasNext(&it)) {
+        corto_result *r = corto_iter_next(&it);
+        corto_id id;
+        char *lib;
+        sprintf(id, "%s/%s", r->parent, r->id);
+        if ((lib = corto_locate(id, NULL, CORTO_LOCATION_LIB))) {
+            corto_log("#[bold]%s #[grey]=>#[normal] #[grey]%s\n",
+                id, lib);
         } else {
-            corto_lasterr();
-            printf("no packages to list\n");
+            corto_raise(); /* Raise exception if it was thrown */
         }
-
-        if (packages) {
-            corto_loadFreePackages(packages);
-        }
-    } else {
-        corto_iter it;
-        corto_int32 count = 0, globalCount = 0;
-        if (corto_select("//").type("/corto/vstore/package").iter(&it)) {
-            goto error;
-        }
-        while (corto_iter_hasNext(&it)) {
-            corto_result *r = corto_iter_next(&it);
-            corto_id id; sprintf(id, "%s/%s", r->parent, r->id);
-            char *lib = corto_locate(id, NULL, CORTO_LOCATION_LIB);
-            if (lib) {
-                if (locate) {
-                    char *env = corto_locate(id, NULL, CORTO_LOCATION_ENV);
-                    corto_bool isGlobal = !strcmp(env, "/usr/local");
-                    count ++;
-                    if (isGlobal) globalCount ++;
-                    printf("%s%s%s  %s=>%s  %s\n", 
-                        CORTO_CYAN, id, CORTO_NORMAL, 
-                        isGlobal ? CORTO_GREEN : CORTO_MAGENTA, CORTO_NORMAL,
-                        lib);
-                } else {
-                    printf("%s\n", id);
-                }
-            } else if (corto_lasterr()) {
-                printf("%s%s%s%s\n", 
-                    CORTO_RED, id, CORTO_NORMAL, corto_lasterr());                
-            }
-            corto_dealloc(lib);
-        }
-        if (locate) {
-            printf("\nTotal: %d packages (%d %sglobal%s, %d %slocal%s)\n", 
-                count, 
-                globalCount, CORTO_GREEN, CORTO_NORMAL, 
-                count - globalCount, CORTO_MAGENTA, CORTO_NORMAL);
-        }
+        count ++;
+        corto_dealloc(lib);
     }
 
-    corto_argclean(data);
+    corto_log("\n#[white]Total: %d packages\n",
+        count);
 
     return 0;
 error:
-    corto_error("%s", corto_lasterr());
+    corto_raise();
     return -1;
 }
-
